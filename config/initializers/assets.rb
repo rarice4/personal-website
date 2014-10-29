71dad1e604a554f32ce63653a9dce67029241885
Robert Rice <rarice@g.cofc.edu>
2014-10-29 10:47:34 -0400

    web

---
+++ config/initializers/assets.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.precompile += %w( fontawesome-webfont.eot )
+Rails.application.config.assets.precompile += %w( fontawesome-webfont.woff )
+Rails.application.config.assets.precompile += %w( fontawesome-webfont.ttf )
+Rails.application.config.assets.precompile += %w( fontawesome-webfont.svg )
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+# Rails.application.config.assets.precompile += %w( search.js )
